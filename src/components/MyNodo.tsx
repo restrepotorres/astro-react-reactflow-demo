2a96697d31baa0fe7e8e8de0366bf26e85a6b459
restrepoTorres <vmanuel.restrepo@udea.edu.co>
2024-09-17 12:37:18 -0500

    end

---
+++ src/components/MyNodo.tsx
@@ -0,0 +1,19 @@
+import React, {useState} from 'react'
+
+type nodoprops = { readonly nombre: string; readonly creditos: number };
+function MyNodo({ nombre, creditos }: nodoprops): JSX.Element {
+  return (
+    <>
+      <h1
+        onClick={() => {
+          alert(`holi esta materia se llama ${nombre}`);
+        }}
+      >
+        {nombre}
+      </h1>
+      <div> creditos: {creditos}</div>
+    </>
+  );
+}
+
+export default MyNodo;
